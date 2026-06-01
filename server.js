58bdcb444146e6105141ebe022da39fa6ef607c9
Aguilera Milton <maguilera@adelca.com>
2026-05-31 21:59:29 -0500

    fix: eliminar url.parse deprecado en server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const { createServer } = require("http");
-const { parse } = require("url");
 const next = require("next");
 
 const port = parseInt(process.env.PORT, 10) || 3000;
@@ -8,8 +7,7 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
+    handle(req, res);
   }).listen(port, (err) => {
     if (err) throw err;
     console.log(`> Ready on port ${port}`);
